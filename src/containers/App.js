33a5a840d2647081784decb5fdfeea91b2eabdb2
Bob Farrell <bobfarrell@Bobs-MBP.attlocal.net>
2021-04-16 12:12:09 -0500

    Updated files

---
+++ src/containers/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+import { robots } from '../robots';
+import { render } from '@testing-library/react';
+import './App.css';
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+ //   componentDidMount() {
+ //       fetch('https://jsonplaceholder.typicode.com/users')
+ //       .then(response => response.json())
+ //       .then(users =>  this.setState({ robots: users}));
+ //   }
+
+
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render() {
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+        if (robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+    return (
+        <div className='tc'>
+            <h1  className='f1'>RoboCards</h1>
+            <SearchBox searchChange={this.onSearchChange}/>
+            <Scroll>
+                <ErrorBoundry>
+            <CardList robots={filteredRobots}/>
+                </ErrorBoundry>
+            </Scroll>
+        </div>
+    );
+    }
+}
+}
+
+export default App;
---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'John Smith',
+        username: 'jsmith',
+        email: 'jsmith@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'James Davis',
+        username: 'jdavis',
+        email: 'jdavis@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Mike Jones',
+        username: 'mjones',
+        email: 'mjones@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Joe Schmoe',
+        username: 'jschmoe',
+        email: 'jschmoe@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Don Johnson',
+        username: 'djohnson',
+        email: 'djohnson@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'David Clark',
+        username: 'dclark',
+        email: 'dclark@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Tom Peterson',
+        username: 'tpeterson',
+        email: 'tpeterson@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Jason Henry',
+        username: 'jhenry',
+        email: 'jhenry@gmail.com'
+    }
+];
